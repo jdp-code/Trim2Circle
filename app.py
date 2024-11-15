ea38b9746c3dd9780d39e6ac078146ed8c482438
Jonas David Penner <jonasdaviddev@gmail.com>
2024-11-15 14:27:34 +0100

    Change Port

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def create_pdf(images, diameter_mm, buffer):
     c.save()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5500, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
